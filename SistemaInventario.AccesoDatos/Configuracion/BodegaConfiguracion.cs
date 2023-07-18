9392dda64a06cc57901e0b7ddbdf6f9a5647d70e
eiderjulio <eder.julio2060@hotmail.com>
2023-07-17 20:52:53 -0500

    primer cargue del proyecto

---
+++ SistemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Configuracion
+{
+    public class BodegaConfiguracion : IEntityTypeConfiguration<Bodega>
+    {
+        public void Configure(EntityTypeBuilder<Bodega> Builder)
+        {
+            Builder.Property(x => x.Id).IsRequired();
+            Builder.Property(x => x.Nombre).IsRequired().HasMaxLength(60);
+            Builder.Property(x => x.Descripcion).IsRequired().HasMaxLength(100);
+            Builder.Property(x => x.Estado).IsRequired();
+
+        }
+
+    }
+}
